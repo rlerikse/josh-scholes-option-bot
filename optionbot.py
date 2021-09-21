ef6eade6ef926c5b5fcc8fa69a91fc7496dd3add
Robert Eriksen <rlerikse@mtu.edu>
2021-09-21 14:15:39 -0400

    Updated dependencies, added config file to git ignor

--- optionbot.py
+++ optionbot.py
@@ -1,82 +1,93 @@
 import requests
 import robin_stocks
 import json
+import config
+import robin_stocks.robinhood as r
+from robin_stocks import *
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-loginRequest = robin_stocks.authentication.login(username='XXXXXXXXXX', password='XXXXXXXXXXXX', expiresIn=86400,scope='internal', by_sms=True, store_session=True)
+loginRequest = r.login(config.username, config.password, expiresIn=86400,scope='internal', by_sms=True, store_session=True)
+# loginRequest = r.login(config.username, config.password)
+
 TOKEN = loginRequest['access_token']
-profileRequest = robin_stocks.profiles.load_portfolio_profile(info=None)
+profileRequest = r.profiles.load_portfolio_profile(info=None)
 # print(profileRequest)
+while 1:
+    inputSymbols = input("Enter ticker (SPY):")
+    if inputSymbols == "exit":
+        break
+    n = int(input("How many dates?: "))
+    arr = str(input("Enter dates seperated by commas (2021-10-25, ...):"))   # takes the whole line of n numbers
+    expirationDates = list(map(str,arr.split(','))) # split those numbers with space( becomes ['2','3','6','6','5']) and then map every element into int (becomes [2,3,6,6,5])
+    # expirationDates = input('2021-10-25','2021-10-27','2021-10-30','2021-10-22']
+    print(expirationDates)
+    amountITM = 1
+    volumeFloor = 50
+    askMax = 100            #x100
+    perc_change_max = 10        #of underlying
+    percent_gain_wanted = 100
 
-inputSymbols = ['SPY']
-expirationDates = ['2020-03-25','2020-03-27','2020-03-30','2020-03-31']
-amountITM = 1
-volumeFloor = 50
-askMax = .19            #x100
-perc_change_max = 10        #of underlying
-percent_gain_wanted = 100
-
-# START OF NO TOUCHY ZONE
-
-
-for expirationDate in expirationDates:
-    print("STARTING OPTION SEARCH FOR " + expirationDate)
-    availableOptions = robin_stocks.options.find_options_for_list_of_stocks_by_expiration_date(inputSymbols, expirationDate, optionType='both', info=None)
-    # jprint(availableOptions)
-    if availableOptions != None:
-        for d in availableOptions:
-            ask_price = float(d['ask_price'])
-            ask_size = int(d['ask_size'])
-            break_even = float(d['break_even_price'])
-            implied_volatility = d['implied_volatility']
-            # last_trade_price = float(d['last_trade_price'])
-            volume = int(d['volume'])
-            strike_price = float(d['strike_price'])
-            type = d['type']
-            tradability = d['tradability']
-            url = d['url']
-            chain_symbol = d['chain_symbol']
-            expiration_date = d['expiration_date']
-            current_price_list = robin_stocks.stocks.get_latest_price(chain_symbol, includeExtendedHours=True)
-            current_price = float(current_price_list[0])
-            if volume > volumeFloor:
-                if ask_price < askMax:
-                        if type == 'call':
-                            price_to_reach = strike_price + amountITM
-
-                        if type == 'put':
-                            price_to_reach = strike_price - amountITM
+    # START OF NO TOUCHY ZONE
 
 
-                        price_dif = price_to_reach - current_price
-                        perc_change = price_dif/current_price
-                        perc_gain = amountITM/ask_price
-                        if(perc_gain > percent_gain_wanted/100):
-                            if(abs(perc_change) < perc_change_max/100):
-                                print("")
-                                print(chain_symbol + " ", strike_price, " " + type + " x", ask_size, "Exp: " + expiration_date)
-                                print("Strike price:  ", strike_price)
-                                print("Current price: ", current_price)
-                                print("Ask:           ", ask_price)
-                                print("Ask Size:      ", ask_size)
-                                print("")
-                                print("Url: "+ url)
-                                print("")
-                                print("Price Difference between Strike and Actual: ", price_dif)
-                                print("Price to reach: ", price_to_reach)
-                                print("Percent change to get there", perc_change*100, "%")
-                                print("Percent gain $1 ITM", perc_gain*100, "%")
-                                print("")
-                                print("")
-                                print("")
+    for expirationDate in expirationDates:
+        print("STARTING OPTION SEARCH FOR " + expirationDate)
+        availableOptions = r.find_options_by_expiration(inputSymbols, expirationDate, optionType='both', info=None)
+        jprint(availableOptions)
+        if availableOptions != None:
+            for d in availableOptions:
+                ask_price = float(d['ask_price'])
+                ask_size = int(d['ask_size'])
+                break_even = float(d['break_even_price'])
+                implied_volatility = d['implied_volatility']
+                # last_trade_price = float(d['last_trade_price'])
+                volume = int(d['volume'])
+                strike_price = float(d['strike_price'])
+                type = d['type']
+                tradability = d['tradability']
+                url = d['url']
+                chain_symbol = d['chain_symbol']
+                expiration_date = d['expiration_date']
+                current_price_list = r.stocks.get_latest_price(chain_symbol, includeExtendedHours=True)
+                current_price = float(current_price_list[0])
+                if volume > volumeFloor:
+                    if ask_price < askMax:
+                            if type == 'call':
+                                price_to_reach = strike_price + amountITM
 
-# optionsRequest = robin_stocks.options.get_list_market_data('SPY', expirationDate, info=None)
-# option = robin_stocks.options.get_option_market_data('SPY', expirationDate, , info=None)
-# allOptions = robin_stocks.options.get_market_options(info=None)
+                            if type == 'put':
+                                price_to_reach = strike_price - amountITM
+
+
+                            price_dif = price_to_reach - current_price
+                            perc_change = price_dif/current_price
+                            perc_gain = amountITM/ask_price
+                            if(perc_gain > percent_gain_wanted/100):
+                                if(abs(perc_change) < perc_change_max/100):
+                                    print("")
+                                    print(chain_symbol + " ", strike_price, " " + type + " x", ask_size, "Exp: " + expiration_date)
+                                    print("Strike price:  ", strike_price)
+                                    print("Current price: ", current_price)
+                                    print("Ask:           ", ask_price)
+                                    print("Ask Size:      ", ask_size)
+                                    print("")
+                                    print("Url: "+ url)
+                                    print("")
+                                    print("Price Difference between Strike and Actual: ", price_dif)
+                                    print("Price to reach: ", price_to_reach)
+                                    print("Percent change to get there", perc_change*100, "%")
+                                    print("Percent gain $1 ITM", perc_gain*100, "%")
+                                    print("")
+                                    print("")
+                                    print("")
+
+# optionsRequest = robin_stocks.robinhood..options.get_list_market_data('SPY', expirationDate, info=None)
+# option = robin_stocks.robinhood..options.get_option_market_data('SPY', expirationDate, , info=None)
+# allOptions = robin_stocks.robinhood..options.get_market_options(info=None)
 # jprint(allOptions)
 
 
